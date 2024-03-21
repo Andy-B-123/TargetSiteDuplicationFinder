b81d67d99815c518eeab1657a803e3233f01dfe4
Andy-B-123 <35752341+Andy-B-123@users.noreply.github.com>
2024-03-22 10:13:45 +1100

    Update setup.py
    Trying a setup script

--- setup.py
+++ setup.py
@@ -6,16 +6,19 @@ setup(
     description='Description of your script',
     author='Your Name',
     author_email='your@email.com',
-    url='https://github.com/yourusername/yourrepository',
+    url='https://github.com/Andy-B-123/TargetSiteDuplicationFinder',
     packages=find_packages(),
     install_requires=[
         'tqdm',
         'pysam',
-        'numpy'
+        'numpy',
+        'icecream',
+        'pandas'
+        
     ],
     entry_points={
         'console_scripts': [
-            'your_script_name = your_module_name:main',
+            'your_script_name = TSD_parser.cluster_wrapped:main',
         ],
     },
 )
